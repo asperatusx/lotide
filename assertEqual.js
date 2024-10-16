1d4635cf63e245715dbc3f72791e5ab4e4340334
Andrew <andrewlang.p@gmail.com>
2024-10-15 21:09:36 -0400

    update assertEqual function to match others

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  if (actual !== expected) console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  else console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
 };
+
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
