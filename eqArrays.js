0d5ed71e8ad7d6ab56fbc1856da8874a897b0d2a
Andrew <andrewlang.p@gmail.com>
2024-10-21 22:50:26 -0400

    refactor assertArraysEqual for test files

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+// This function checks if two arrays are equal
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false
   for (let i = 0; i < array1.length; i++) {
