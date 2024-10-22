c0388304aeb5c76091c984f43f5e7bef52afa76e
Andrew <andrewlang.p@gmail.com>
2024-10-21 22:45:37 -0400

    refactor eqArrays.js for test files

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false
   for (let i = 0; i < array1.length; i++) {
@@ -11,8 +6,4 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
