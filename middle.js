797039e5fc3d23312b27fa9956813200251814ec
Andrew <andrewlang.p@gmail.com>
2024-10-21 22:54:11 -0400

    refactor middle for test files

--- middle.js
+++ middle.js
@@ -1,16 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false
-  }
-  return true;
-}
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-}
-
 const middle = function(array) {
   let middleIndex;
   if (array.length <= 2) return []
@@ -25,7 +12,4 @@ const middle = function(array) {
   }
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+module.exports = middle;
