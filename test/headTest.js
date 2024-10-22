1fab5f583a2eb68d285782c5e793f5b66cef42c1
Andrew <andrewlang.p@gmail.com>
2024-10-21 23:15:13 -0400

    Revert "setup mocha and chai for headTest"
    This reverts commit 8b9ef46a88af62b1d506f74446bc9aef14dca9a6.

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,8 @@
 const head = require('../head');
-const assert = require('chai').assert;
+const assertEqual = require('../assertEqual')
 
-describe('#head', () => {
-  it('returns 1 for [1, 2, 3]', () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
-  });
-  it('returns "5" for ["5"]', () => {
-    assert.strictEqual(head(['5']), '5');
-  });
-  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  });
-  it('should return undefined if provided empty array', () => {
-    assert.strictEqual(head([]), undefined);
-  });
-});
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head([3]), 5);
+assertEqual(head([3]), 3);
