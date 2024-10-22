271dc017bcb2d3072135829f1466ff5fa37b9150
Andrew <andrewlang.p@gmail.com>
2024-10-21 22:33:26 -0400

    refactor assertEqual and head to include seperate test files

--- assertEqual.js
+++ assertEqual.js
@@ -3,6 +3,4 @@ const assertEqual = function(actual, expected) {
   else console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
 };
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
