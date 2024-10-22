8b9ef46a88af62b1d506f74446bc9aef14dca9a6
Andrew <andrewlang.p@gmail.com>
2024-10-21 23:09:25 -0400

    setup mocha and chai for headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,17 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([3]), 5);
-assertEqual(head([3]), 3);
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it('should return undefined if provided empty array', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
